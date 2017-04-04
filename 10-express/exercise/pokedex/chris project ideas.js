b7d5e1e738c7fa3a50f62b13f77f9b9a70b8c9d9
Chris Bowerbank <misterbowerbank@gmail.com>
2017-04-04 18:31:43 -0400

    adding project ideas!

---
+++ 10-express/exercise/pokedex/chris project ideas.js
@@ -0,0 +1,15 @@
+// Chris Bowerbank final project ideas:
+//
+// 1) Premier League table (standings)
+//
+// This table will scrape data from various sites and autoupdate based on ongoing games in England. When a user clicks on an individual row of the table they'll see upcoming games, previous results, and more.
+//
+//
+// 2) Spotify local mood
+//
+// This application will build a playlist of songs in spotify that people around you are listening to. For example, if it's a rainy day, the chances are you'll see a playlist of a bunch of depressing love songs.
+//
+//
+// 3) Book recommendation tool
+//
+// Pulling goodreads book data and finding a better way to recommend books than what current logic exists in goodreads' model. Goodreads currently recommends books in the same topic area with good ratings. I'd like to cross the genre boundary and recommend books that could possibly align with user preferences but also allow them to branch out of their preferred topic areas.
